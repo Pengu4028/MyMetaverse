40351ab08ea710fb71bb835658fdd360da156cae
Pengu4028 <kga4028@gmail.com>
2025-07-28 13:27:11 +0900

    [FIx]스택 오류수정중

---
+++ Assets/Stack/Scripts/BaseUI.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace Stack { 
+
+    public abstract class BaseUI : MonoBehaviour
+    {
+        protected UIManager uiManager;
+
+
+        public virtual void Init(UIManager uiManager)
+        {
+            this.uiManager = uiManager;
+        }
+
+        protected abstract UIState GetUIState();
+        public void SetActive(UIState state)
+        {
+            this.gameObject.SetActive(GetUIState() == state);
+        }
+    }
+
+}
